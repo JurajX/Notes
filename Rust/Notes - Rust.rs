dd8b0675e126aa17d143b7a3dd0fdd33179acda9
Juraj <37395233+JurajX@users.noreply.github.com>
2024-01-15 17:14:26 +0100

    add more Rust notes

--- Rust/Notes - Rust.rs
+++ Rust/Notes - Rust.rs
@@ -1,6 +1,33 @@
 //==================== ==================== ToC
+// === Printing Macros
+// === Declarations
+// === Expressions and Statements
+// === Type Aliases
+// === Integer
+// === Floats
+// === Boolean
+// === Character
+// === Tuple
+// === Arrays, Vectors, Slices
+// === String Types
+// === References
+// === Box and Reference Counts
+// === Raw Pointers
+// === Functions
+// === Ownership
+// === Structs
+// === Enums
+// === Optionals and Errors
+// === Pattern Matching
+// === Traits
+// === Statements
+// === Operators
+// === Keywords
 
 
+//==================== Printing Macros
+println!("{} < {}", 10, 20);        // prints "10 < 20" to stdout
+dbg!("haha");                       // prints debug info to stderr, takes and returns ownership, unless passed by reference
 
 //==================== Declarations
 let var:type;               // declares an immutable var of type (can be assigned only once)
@@ -17,7 +44,7 @@ const CONST:type = 3.14;    // declares an immutable constant; type cannot be om
 
 //==================== Expressions and Statements
 4 + 5               // is an expression; returns a value
-4 + 5;              // is astatement; does not return a value
+4 + 5;              // is a statement; does not return a value
 let x = 4 + 5;      // is a statement; does not return a value
 
 {                   // start of a block
@@ -41,7 +68,7 @@ isize, usize                // architecture size (the same size as an address) s
 
 //---------- literals
 // _ can be used as visual separator to make numbers more readable
-1_i32, 1i32                 // represents 1 as i32
+1_i32, 1i32 // represents 1 as i32
 98_222      // decimal
 0xff        // hex
 0o77        // octal
@@ -49,7 +76,7 @@ isize, usize                // architecture size (the same size as an address) s
 
 //---------- byte (u8) literals
 b'A'        // byte literals of type u8 (only for ASCII)
-// special byte literals
+            // special byte literals
 b'\''       // Single quote     39u8
 b'\\'       // Backslash        92u8
 b'\n'       // Newline          10u8
@@ -57,8 +84,8 @@ b'\r'       // Carriage return  13u8
 b'\t'       // Tab               9u8
 
 //---------- declarations
-let int: u32 = 0xff;        // int is 15
-let mut int: u8 = 0xff;     // int is mutable and equal to 15
+let int: u32 = 0xf;        // int is 15
+let mut int: u8 = 0xf;     // int is mutable and equal to 15
 
 //---------- members
 // XXX can be add, sub, mul, div, rem, neg, abs, pow, shl, shr
@@ -114,7 +141,7 @@ let mut b = false;          // b is mutable and equal to false
 char                        // unicode 32-bit wide; ranges from U+0000 to U+D7FF and U+E000 to U+10FFFF inclusive
 
 //---------- declarations
-let ch: char = 'A';         // cha is a 32-bit long character holding letter A
+let ch: char = 'A';         // ch is a 32-bit long character holding letter A
 let mut ch = 'B';           // as above, just mutable and holding B
 
 
@@ -134,46 +161,6 @@ let tup: () = ();                           // a special tuple called 'unit'; re
 
 
 
-//==================== References
-// non-owning pointers that must not outlive their referent
-//---------- types
-&T;                         // a reference to type T; allows for multiple readers
-&mut T;                     // a mutable references; prohibits having any other way accessing the value (single writer)
-
-//---------- declarations
-let r: &T = &x;             // r stores a reference to the variable x
-let rr: &&T = &r;           // rr stores a reference to a reference to variable x; can be chained undefinitely
-*r;                         // dereferencing a reference
-r.member;                   // the dot operator automatically dereferences the reference; i.e. (*r).member
-r1 == r2; rr1 == rr2;       // both compare values (as many dereferences as needed are applied); to compare addresses use std::ptr::eq
-r1 == rr1;                  // this is not allowed
-
-//----------
-
-
-
-//==================== Box and Reference Counts
-//---------- types
-Box<T>                      // a smart pointer owning pointer to value on the heap (unique_ptr)
-std::rc::Arc<T>;            // atomic reference count, used for thread-safe code
-std::rc::Rc<T>;             // reference count, used for non-thread-safe code (shared_ptr)
-std::rc::Weak<T>;           // weak pointer; (weak_ptr)
-
-//---------- declarations
-let t = (12, "eggs");       // defines a tuple
-let b = Box::new(t);        // allocates a tuple in the heap
-
-let s: Rc<String> = Rc::new("str".to_string()); // creates a string rc pointer; read-only; behaves as a variable
-let t: Rc<String> = s.clone();                  // increases a reference count
-
-
-
-//==================== Raw Pointers
-// can be dereferenced only within an 'unsafe' block
-*mut T, *const T;           // mutable or constant raw pointer; works as in c++
-
-
-
 //==================== Arrays, Vectors, Slices
 //---------- types
 [T; N];                     // type denoting a const size array of length N with values of type T
@@ -188,8 +175,11 @@ Vec::<i32>::with_capacity(N);       // use to allocate capacity at creation
 let v: Vec<i32> = (1..2).collect(); // creates a vector containing [1, 2]
 let v: Vec<i32> = vec![1, 2];       // creates a vector containing [1, 2]
 let v: Vec<i32> = vec![5; 2];       // creates a vector containing [5, 5]
+let vr: &[i32] = &v[1..3];          // creates a slice of length 2, starting at 1-st (0-indexed) element
+let vr: &[i32] = &v[1..v.len()];    // creates a slice starting at 1-st (0-indexed) element
+let vr: &[i32] = &v[1..];           // as above
 let vr: &[i32] = &v;                // creates a slice starting at 0-th (0-indexed) element
-let vr: &[i32] = &v[1..];           // creates a slice starting at 1-st (0-indexed) element
+let vr: &[i32] = &v[..];            // as above
 
 //---------- indexing
 v[i];                       // i-th element of v; i must be 'usize' and in [ 0, v.len() - 1 ]
@@ -224,10 +214,43 @@ r"raw string";              // everything inside is included verbatim; r##"alter
 
 
 
-//==================== Optionals and Errors
+//==================== References
+// non-owning pointers that must not outlive their referent
 //---------- types
-Option<&str>                // optional value, either None or Some(value); e.g. 'None' or 'Some("Dr.")'
-Result<u64, Error>          // result of op. that may fail, either Ok(value) or Err(e); e.g. 'Ok(123)' or 'Err(Error::last_os_error())'
+&T;                         // a reference to type T; allows for multiple readers
+&mut T;                     // a mutable references; prohibits having any other way accessing the value (single writer)
+
+//---------- declarations
+let r: &T = &x;             // r stores a reference to the variable x
+let rr: &&T = &r;           // rr stores a reference to a reference to variable x; can be chained undefinitely
+*r;                         // dereferencing a reference
+r.member;                   // the dot operator automatically dereferences the reference; i.e. (*r).member
+r1 == r2; rr1 == rr2;       // both compare values (as many dereferences as needed are applied); to compare addresses use std::ptr::eq
+r1 == rr1;                  // this is not allowed
+
+//----------
+
+
+
+//==================== Box and Reference Counts
+//---------- types
+Box<T>                      // a smart pointer owning pointer to value on the heap (unique_ptr)
+std::rc::Arc<T>;            // atomic reference count, used for thread-safe code
+std::rc::Rc<T>;             // reference count, used for non-thread-safe code (shared_ptr)
+std::rc::Weak<T>;           // weak pointer; (weak_ptr)
+
+//---------- declarations
+let t = (12, "eggs");       // defines a tuple
+let b = Box::new(t);        // allocates a tuple on the heap
+
+let s: Rc<String> = Rc::new("str".to_string()); // creates a string rc pointer; read-only; behaves as a variable
+let t: Rc<String> = s.clone();                  // increases a reference count
+
+
+
+//==================== Raw Pointers
+// can be dereferenced only within an 'unsafe' block
+*mut T, *const T;           // mutable or constant raw pointer; works as in c++
 
 
 
@@ -248,11 +271,16 @@ fn(&str) -> bool            // pointer to function taking a string and returning
 
 //==================== Ownership
 
-//---------- Ownership and Functions
-fn fct(s: String) {/*...*/};        // a fn taking a string
-let s1 = String::from("hello");     // creates a string
-let s2 = s1;                        // moves s1 into s2; s1 cannot be used after this point
-fct(s2);                            // moves s2 into the fct; s2 cannot be used after this point
+//---------- Assignment and Functions
+fn fct(s: String) -> String {/*...*/};  // a fn taking a string
+let s1 = String::from("hello");         // creates a string
+let s2 = s1;                            // moves s1 into s2; s1 cannot be used after this point
+let s3 = fct(s2);                       // moves s2 into the fct, s2 cannot be used after this point; s3 contain the string moved from the fct
+
+//---------- Cloning
+let s1 = String::from("hello");
+let s2 = s1.clone();
+println!("s1 = {}, s2 = {}", s1, s2);
 
 //---------- References and Borrowing
 // const reference
@@ -269,8 +297,8 @@ fct(&mut s1);                       // passes a reference; does not move s1 anyw
 // Mutable references cannot coexist with other mutable or immutable references to the same variable!
 let r1 = &s; // no problem
 let r2 = &s; // no problem
-let r3 = &mut s; // BIG PROBLEM
-let r4 = &mut s; // BIG PROBLEM
+let r3 = &mut s; // BIG PROBLEM, immutable references already exist
+let r4 = &mut s; // BIG PROBLEM, both mutable and immutable references aready exist
 
 // Reference's scope lasts untill its last ussage (Non-Lexical Lifetime)
 let r1 = &s;                        // no problem
@@ -282,13 +310,14 @@ println!("{}", r3);
 
 
 //==================== Structs
+#[derive(Debug)]                    // kinda decorator; helps with printing the struct with println!()
 struct Rectangle {                  // struct definition
-    member: i64,                    // member field and its type
-    number: u64,                    // member field and its type
+    width: i64,                     // member field and its type
+    height: u64,                    // member field and its type
 }
 
 impl Rectangle {                    // implementation block for associated functions; multiple impl blocks are allowed
-    fn area(&self) -> u32 {         // member function
+    fn area(&self) -> u32 {         // member function; shorthand for area(self: &Self), i.e. it borrows a reference
         self.width * self.height    // returns the expression (no semicolon); equivalent to return ...;
     }
     fn square(size: u32) -> Self {  // 'constructor' for a square rectangle; Self is alias for Rectangle
@@ -298,21 +327,28 @@ impl Rectangle {                    // implementation block for associated funct
     }
 }
 
-let st1 = Rectangle {                // instantiation of a struct
-        member: 1,                  // assigning to the member
-        number: 1,                  // assigning to the number
+let st1 = Rectangle {               // instantiation of a struct
+        width: 1,                   // assigning to the width
+        height: 1,                  // assigning to the height
 };
-st1.number;                         // accessing the member
+st1.width;                          // accessing the member width
+println!("{:#?}", st1)              // displays "Rectangle { width: 30, height: 50 }"; connected to #[derive(Debug)]
+println!("{:?}", st1)               // displays "Rectangle { width: 30, height: 50 }" with each memeber on a new line
 
 let st2 = Rectangle {                // another instance of struct
-    member: 10,                     // assigning to the member
-    ..st1                           // move the remaining fields from st1; st1 no longer usable
+    width: 10,                      // assigning to the width
+    ..st1                           // move the remaining fields from st1; st1 no longer usable if it had heap allocated data
 };
 
 let sq = Rectangle::square(3);      // using the 'constructor'
 
-#[derive(Debug)]                    // kinda decorator; helps with printing the struct with println!()
-struct Another {/*...*/}            // just cefinition
+
+fn build_rect(width: i64, height: u64) -> Rectangle {
+    Rectangle (                     // the rectangle instance will be moved
+        width: width,               // assigning the width
+        height                      // shorthand for height: height, only possible with same-name params
+    )
+}
 
 //---------- Tuple Structs
 struct Color(i32, i32, i32);        // define a tuple struct; i.e. a named tuple
@@ -324,10 +360,68 @@ struct AlwaysEqual;                 // does not have any field
 
 
 //==================== Enums
-enum Attend {               // enumeration
-    OnTime,                 // elements can be empty
+enum Attend {               // enumeration value can only be one of its variants
+    OnTime,                 // elements/variants can be empty
     Late(u32)               // ... or hold a value
-};
+}
+
+enum Message {                      // enumeration value can only be one of its variants
+    Quit,                           // has no data associated with it
+    Move { x: i32, y: i32 },        // has named fields, like a struct does
+    Write(String),                  // includes a single String
+    ChangeColor(i32, i32, i32),     // includes three i32 values
+}
+
+impl Message {                      // implementation block for member functions
+    fn call(&self) {/*...*/}
+}
+let m = Message::Write(String::from("hello"));
+m.call();
+
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+let home = IpAddr::V4(127, 0, 0, 1);
+let loopback = IpAddr::V6(String::from("::1"));
+
+
+
+//==================== Optionals and Errors
+Option<&str>                // optional value, either None or Some(value); e.g. 'None' or 'Some("Dr.")'
+Result<u64, Error>          // result of op. that may fail, either Ok(value) or Err(e); e.g. 'Ok(123)' or 'Err(Error::last_os_error())'
+
+
+
+//==================== Pattern Matching
+enum UsState {Alabama/*,...*/}
+enum Coin { Penny, Nickel, Dime, Quarter(UsState)}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+        other => do_sth(other),     // placed at the end, calls a specific fn on other values (not aplicable here)
+        _ => dont_even_catch(),     // placed at the end, default action for all other values (not aplicable here)
+        _ => (),                    // placed at the end, do nothing for all other values (not aplicable here)
+    }
+}
+
+// the following is a shortcut for pattern matching below
+let config_max = Some(3u8);
+if let Some(max) = config_max {
+    println!("The maximum is configured to be {}", max);
+}
+// the pattern matching version
+let config_max = Some(3u8);
+match config_max {
+    Some(max) => println!("The maximum is configured to be {}", max),
+    _ => (),
+}
 
 
 
